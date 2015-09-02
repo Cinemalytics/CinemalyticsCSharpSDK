6c21b854b0dd616f941b43523ea6e8692d7a9e93
arjit <arjit@c01aec6c-8961-441e-8364-a2f019d9c4c9>
2015-09-02 15:41:17 +0000

    Adding json parser

--- CinemalyticsCSharpSDK/Cinemalytics.cs
+++ CinemalyticsCSharpSDK/Cinemalytics.cs
@@ -7,5 +7,15 @@ namespace CinemalyticsCSharpSDK
 {
     public class Cinemalytics
     {
+        private static String AUTH_TOKEN = "";
+
+        /// <summary>
+        /// Constructs an instance with provided authToken
+        /// </summary>
+        /// <param name="authToken"></param>
+        public Cinemalytics(String authToken)
+        {
+            AUTH_TOKEN = authToken;
+        }
     }
 }
